95e15337350d20119b0def0ff4356ae688bcb8c4
ghenriqu-dev <ghenriqu.dev@gmail.com>
2023-02-03 01:10:34 -0300

    configure .env & models

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,5 +1,8 @@
-const getAll = () => {
+const tasksModel = require('../models/tasksModel')
 
+const getAll = async (request, response) => {
+    const tasks = await tasksModel.getAll()
+    return response.status(200).json(tasks)
 }
 
 module.exports = {
