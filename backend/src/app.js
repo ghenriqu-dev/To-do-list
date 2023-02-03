1604584b156e7d05bb73c4ce1c77f392a43fdec1
ghenriqu-dev <ghenriqu.dev@gmail.com>
2023-02-02 23:49:08 -0300

    add router file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
+const router = require('./router')
 const app = express()
 
-app.get('/', (request, response) => {response.status(200).send('Hello, World!')})
-
+app.use(router)
 module.exports = app
