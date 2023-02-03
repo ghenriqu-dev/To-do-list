2999870d706d5098133d321fa01620e126a3b7b7
ghenriqu-dev <ghenriqu.dev@gmail.com>
2023-02-03 00:31:18 -0300

    add tasksController.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const tasksController = require('./controllers/tasksController')
 const router = express.Router()
 
-router.get('/tasks', (req, res) => {res.status(200).send('Router Funcionando!')})
+router.get('/tasks', tasksController.getAll)
 
 module.exports = router
