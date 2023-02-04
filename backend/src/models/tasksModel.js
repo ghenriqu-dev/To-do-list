2078b80805d0fa72814326609c2edc822dcfdf34
ghenriqu-dev <ghenriqu.dev@gmail.com>
2023-02-04 00:25:36 -0300

    create deleteTask, putTask

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,4 +1,5 @@
 const connection = require('./connection')
+
 const getAll = async () => {
     const [tasks] = await connection.execute('SELECT * FROM tasks')
     return tasks
@@ -11,10 +12,25 @@ const createTask = async (task) => {
 
     const [createdTask] = await connection.execute(query, [title, 'Pendente', date])
 
-    return {id: createdTask.insertId}
+    return {id: createdTask.insertId, title: task.title}
+}
+
+const deleteTask = async (id) => {
+    await connection.execute('DELETE FROM tasks WHERE id = ?', [id])
+    return id
+}
+
+const updateTask = async (id, task) => {
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?'
+    const { title, status } = task
+
+    const updatedTask = await connection.execute(query, [title, status, id])
+    return updatedTask
 }
 
 module.exports = {
     getAll,
-    createTask
+    createTask,
+    deleteTask,
+    updateTask
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const tasksController = require('./controllers/tasksController')
+const tasksMiddleware = require('./middlewares/tasksMiddleware')
 const router = express.Router()
 
 router.get('/tasks', tasksController.getAll)
-router.post('/tasks', tasksController.createTask)
+router.post('/tasks', tasksMiddleware.validateTitle ,tasksController.createTask)
+router.delete('/tasks/:id', tasksController.deleteTask)
+router.put('/tasks/:id', 
+    tasksMiddleware.validateTitle ,
+    tasksMiddleware.validateStatus, 
+    tasksController.updateTask
+)
 
 module.exports = router
